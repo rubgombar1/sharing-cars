23ff3f163c70099e3de9916991cd31919681a5c4
Ruben Gomez <rubgomez93@gmail.com>
2016-05-15 14:42:51 +0200

    Added list routes

--- sharingcars/routes/urls.py
+++ sharingcars/routes/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from routes.views import RouteCreateView
+from routes.views import RouteCreateView, RouteListView
+from routes.models import Route
 
 urlpatterns = [
     url(r'^create$', RouteCreateView.as_view(), name='create-route'),
+    url(r'^all$', RouteListView.as_view(queryset=Route.objects.order_by('-creationMoment')),
+        name='all-routes'),
 ]
--- sharingcars/routes/views.py
+++ sharingcars/routes/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse_lazy
 
 from routes.models import Route
@@ -20,3 +21,8 @@ class RouteCreateView(CreateView):
         user = User.objects.get(user_account__id=self.request.user.id)
         instance.user = user
         return super(RouteCreateView, self).form_valid(form)
+
+
+class RouteListView(ListView):
+    template_name = 'routes/list.html'
+    model = Route
