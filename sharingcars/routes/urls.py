7619fd5561645ffbdbf5d56aeab9cef5177395aa
Ruben Gomez <rubgomez93@gmail.com>
2016-06-18 17:09:23 +0200

    Fix extends in templates and add my routes and recommendations for user.

--- sharingcars/routes/urls.py
+++ sharingcars/routes/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from routes.views import RouteCreateView
+from routes.views import RouteCreateView, RouteUserListView, RouteUserRecommendationsListView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from routes.models import Route
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^all$', ListView.as_view(queryset=Route.objects.order_by('-creationMoment'),
                                    model=Route, template_name='routes/list.html'),
         name='all-routes'),
+    url(r'^user$', RouteUserListView.as_view(), name='user-routes'),
+    url(r'^recommendations$', RouteUserRecommendationsListView.as_view(), name='recommendations-routes'),
     url(r'^details/(?P<pk>[-\w]+)$', DetailView.as_view(template_name='routes/show.html',
                                                         queryset=Route.objects.all()),
         name='details-route'),
--- sharingcars/routes/views.py
+++ sharingcars/routes/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse_lazy
+from django.db.models import Q
 
 from routes.models import Route
 from common.models import User
@@ -9,7 +11,7 @@ from routes.forms import RouteCreateForm
 class RouteCreateView(CreateView):
     model = Route
     form_class = RouteCreateForm
-    template_name = 'common/registration/register.html'
+    template_name = 'routes/create.html'
     success_url = reverse_lazy('index')
 
     def get_success_url(self):
@@ -20,3 +22,29 @@ class RouteCreateView(CreateView):
         user = User.objects.get(user_account__id=self.request.user.id)
         instance.user = user
         return super(RouteCreateView, self).form_valid(form)
+
+
+class RouteUserListView(ListView):
+    model = Route
+    template_name = 'routes/list.html'
+
+    def get_queryset(self):
+        return Route.objects.filter(user__user_account__id=self.request.user.id)
+
+
+class RouteUserRecommendationsListView(ListView):
+    model = Route
+    template_name = 'routes/list.html'
+
+    def get_context_data(self):
+        context = super(RouteUserRecommendationsListView, self).get_context_data()
+        userId = self.request.user.id
+        userRoutes = Route.objects.filter(user__user_account__id=userId)
+        routes = Route.objects.filter(~Q(user__user_account__id=userId), visibility=1)
+        recommendations = set()
+        for userRoute in userRoutes:
+            for route in routes:
+                if userRoute.origin == route.origin and userRoute.destination == route.destination:
+                    recommendations.add(route)
+        context['route_list'] = recommendations
+        return context
